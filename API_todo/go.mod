993f178027133765b11fe1dc1096f6b59494c14e
katterianjali <saianjali610@gmail.com>
2023-08-17 22:12:32 +0100

    First Server created with external module

---
+++ API_todo/go.mod
@@ -0,0 +1,3 @@
+module API_todo
+
+go 1.21.0
---
+++ API_todo/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"API_todo/structs"
+	"encoding/json"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			data := structs.Response{
+				Code: http.StatusOK,
+				Body: "Ping",
+			}
+			json.NewEncoder(w).Encode(data)
+		}
+	})
+	http.ListenAndServe(":3000", mux)
+
+}
